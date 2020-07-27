1b49ed2a9611776404c2d5c3236341f17c90260b
sahithi0511 <srisahithich@gmail.com>
2020-07-27 09:40:10 +0530

    kept the value dynamic from array size

--- TwoDArray.java
+++ TwoDArray.java
@@ -3,9 +3,9 @@ class TwoDArray
   public static void main(String args[])
  {
    int a[][] = { {2,7,9},{3,6,1},{7,4,2} };
-   for (int i=0; i<3 ; i++) 
+   for (int i=0; i<a.length ; i++) 
    {
-    for (int j=0; j <3 ; j++) 
+    for (int j=0; j <a.length ; j++) 
     System.out.print(a[i][j] + " ");
    System.out.println();
    }
