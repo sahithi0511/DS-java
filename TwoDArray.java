2ef266208db9027d0b5792300240a41f28b552b3
sahithi0511 <srisahithich@gmail.com>
2020-07-27 09:39:10 +0530

    2D Array program

---
+++ TwoDArray.java
@@ -0,0 +1,13 @@
+class TwoDArray
+{
+  public static void main(String args[])
+ {
+   int a[][] = { {2,7,9},{3,6,1},{7,4,2} };
+   for (int i=0; i<3 ; i++) 
+   {
+    for (int j=0; j <3 ; j++) 
+    System.out.print(a[i][j] + " ");
+   System.out.println();
+   }
+ }
+}
